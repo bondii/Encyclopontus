88533152e1d334a8b6335344c1ab3ca85ff54522
Pontus Eriksson <pontus.eriksson@datavarde.se>
2024-01-23 21:56:04 +0100

    Add sprained ankle, improve Barcelona

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       fontFamily: {
         organic: ['"Bad Script"', 'cursive'],
       },
+      boxShadow: {
+        'glow-xl': '0 0 25px -5px rgb(0 0 0 / 0.1)',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
