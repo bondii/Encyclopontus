cd6c211aec0b78efd147d68aa5abc56d985ec2c4
Pontus Eriksson <pontus.eriksson@opac.se>
2025-12-25 09:27:06 +0100

    Add dev stöff like Nix flake

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = with pkgs; [
+                  python313Packages.black
+                  # krb5
+                  # stdenv.cc.cc.lib # native libs: libstdc++.so.6
+                  # openldap
+                  # stdenv.cc.cc.lib
+                ];
+
+                languages.python = {
+                  enable = true;
+                  package = pkgs.python313;
+                  poetry = {
+                    enable = true;
+                    install = {
+                      enable = true;
+                      allExtras = true;
+                    };
+                    activate.enable = true;
+                  };
+                };
+                # enterShell = ''
+                #   export LD_LIBRARY_PATH=${
+                #     pkgs.lib.makeLibraryPath [
+                #       pkgs.krb5
+                #       pkgs.openldap
+                #       pkgs.stdenv.cc.cc.lib
+                #     ]
+                #   }:$LD_LIBRARY_PATH
+                # '';
+
+                process.managers.process-compose.tui.enable = false;
+                dotenv.disableHint = true;
+              }
+            ];
+          };
+        }
+      );
+    };
+}
